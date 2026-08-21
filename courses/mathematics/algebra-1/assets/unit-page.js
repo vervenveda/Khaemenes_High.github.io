c638f417984ad111e34bce2da96258c457cd871d
Jennifer Kay Pearl <vervenveda@proton.me>
2026-08-21 11:12:16 -0400

    A1 draft activate Unit 02 rebuilt content

--- courses/mathematics/algebra-1/assets/unit-page.js
+++ courses/mathematics/algebra-1/assets/unit-page.js
@@ -6,6 +6,7 @@ const ref=window.PAGE_REF||{};
 const unit=Number(ref.unit);
 const scripts=[];
 if(unit===1) scripts.push(new URL("unit-01-content-upgrade.js",base).href);
+if(unit===2) scripts.push(new URL("unit-02-content-upgrade.js",base).href);
 if(unit===11) scripts.push(new URL("unit-11-content-upgrade.js",base).href);
 if(unit===12) scripts.push(new URL("unit-12-content-upgrade.js",base).href);
 if(unit===13) scripts.push(new URL("unit-13-content-upgrade.js",base).href);
