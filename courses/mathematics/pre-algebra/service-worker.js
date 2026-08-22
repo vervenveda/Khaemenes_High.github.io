5fd23f84eb2fa8cc7f5c090580e49f2231aaacf9
Jennifer Kay Pearl <vervenveda@proton.me>
2026-08-22 12:13:29 -0400

    Refresh Pre-Algebra cache after placement architecture fix

--- courses/mathematics/pre-algebra/service-worker.js
+++ courses/mathematics/pre-algebra/service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX="khaemenes-prealgebra-core-";
-const CACHE_NAME=`${CACHE_PREFIX}v1`;
+const CACHE_NAME=`${CACHE_PREFIX}v2-placement-separated`;
 const CORE_FILES=[
   "./",
   "./index.html",
